d6f09de2a1aa7f272e626f82d091667394908e68
Allan Hall <allanhall@Allans-MBP.attlocal.net>
2020-04-02 14:41:19 -0500

    Got page transition to work

--- public/main.js
+++ public/main.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', main)
 
 function pageTransition () {
   var tl = gsap.timeline()
-  tl.to('ul.transition li', { duration: 0.5, scaleY: 1, transformOrigin: 'bottom left', stagger: 0.2 })
-  tl.to('ul.transition li', { duration: 0.5, scaleY: 0, transformOrigin: 'bottom left', stagger: 0.1, delay: 0.1 })
+  tl.to('.transition li', { duration: 0.5, scaleY: 1, transformOrigin: 'bottom left', stagger: 0.2 })
+  tl.to('.transition li', { duration: 0.5, scaleY: 0, transformOrigin: 'bottom left', stagger: 0.1, delay: 0.1 })
 }
 
 function contentAnimation () {
